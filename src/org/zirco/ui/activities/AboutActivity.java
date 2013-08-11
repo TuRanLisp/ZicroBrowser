7b93e9432195f346d4f245c849a710430a85135a
蒋元恒 <nocturnerjyh123@sina.cn>
2013-08-12 03:55:53 +0800

    Update AboutActivity.java

--- src/org/zirco/ui/activities/AboutActivity.java
+++ src/org/zirco/ui/activities/AboutActivity.java
@@ -1,18 +1,3 @@
-/*
- * Zirco Browser for Android
- * 
- * Copyright (C) 2010 J. Devauchelle and contributors.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 3 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-
 package org.zirco.ui.activities;
 
 import org.zirco.R;
@@ -34,33 +19,32 @@ import android.widget.TextView;
 public class AboutActivity extends Activity {
 	
 	 @Override
-	 protected void onCreate(Bundle savedInstanceState) {
-		 super.onCreate(savedInstanceState);
-		 
-		 Window w = getWindow();
-		 w.requestFeature(Window.FEATURE_LEFT_ICON);
-		 
-		 setContentView(R.layout.about_activity);
-		 
-		 w.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,	android.R.drawable.ic_dialog_info);
-		 		 
-		 TextView versionText = (TextView) this.findViewById(R.id.AboutActivity_VersionText);
-		 versionText.setText(this.getString(R.string.AboutActivity_VersionText) + " " + getVersion());
+	 protected void onCreat(Bundle saveInstanceState) {
+	 	super.onCreat(savedInstanceState);
+	 	
+	 	Window w = getWindow();
+	 	w.requestFeature(Window.FEATURE_LEFT_ICON);
+	 	
+	 	setContentView(R.layout.about_activity);
+	 	
+	 	w.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, android.R.drawable.ic_dialog_info);
+	 	
+	 	TextView VersionText = (TextView) this.findViewById(R.id.AboutActivity_VersionText);
+	 	versionText.setText(this.getString(R.string.AboutActivity_VersionText) + " " + getVersion());
+	 	
+	 	TextView licenseText = (TextView) this.findViewById(R.id.AboutActivity_LicenseText);
+	 	licenseText.setText(this.getString(R.string.AboutActivity_LicenseText) + " " + this.getString(R.string.AboutActivity_LicenseTextValue));
+	 	
+	 	TextView urlText = (TextView) this.findViewById(R.id.AboutActivity_UrlText);
+		urlText.setText(this.getString(R.string.AboutActivity_urlTextValue));
+		
+		Button closeBtn = (Button) this.findViewById(R.id.AboutActivity_CloseBtn);
+		closeBtn.setOnClickListener(new View.OnClickListener() {
 			
-		 TextView licenseText = (TextView) this.findViewById(R.id.AboutActivity_LicenseText);
-		 licenseText.setText(this.getString(R.string.AboutActivity_LicenseText) + " " + this.getString(R.string.AboutActivity_LicenseTextValue));		 		 
-		 
-		 TextView urlText = (TextView) this.findViewById(R.id.AboutActivity_UrlText);
-		 urlText.setText(this.getString(R.string.AboutActivity_UrlTextValue));	
-		 
-		 Button closeBtn = (Button) this.findViewById(R.id.AboutActivity_CloseBtn);
-		 closeBtn.setOnClickListener(new View.OnClickListener() {
-
-			 public void onClick(View view) {            	
-				 finish();
-			 }
-
-		 });
+			public void onClick(View view) {
+				finish();
+			}
+		});
 	 }
 	 
 	/**
@@ -83,5 +67,7 @@ public class AboutActivity extends Activity {
 
 		return result;
 	}
+	
+
 
 }
