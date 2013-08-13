2f82c65e88f8f2198f4c254646ee089d839f4eb7
蒋元恒 <nocturnerjyh123@sina.cn>
2013-08-13 18:55:47 +0800

    Update QuickActionGrid.java

--- src/org/greendroid/QuickActionGrid.java
+++ src/org/greendroid/QuickActionGrid.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2010 Cyril Mottier (http://www.cyrilmottier.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.greendroid;
 
 import java.util.List;
@@ -32,14 +17,6 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
-/**
- * A {@link QuickActionGrid} is an implementation of a {@link QuickActionWidget}
- * that displays {@link QuickAction}s in a grid manner. This is usually used to create
- * a shortcut to jump between different type of information on screen.
- *
- * @author Benjamin Fellous
- * @author Cyril Mottier
- */
 public class QuickActionGrid extends QuickActionWidget {
 
 	private GridView mGridView;
@@ -57,24 +34,20 @@ public class QuickActionGrid extends QuickActionWidget {
 	protected void populateQuickActions(final List<QuickAction> quickActions) {
 
 		mGridView.setAdapter(new BaseAdapter() {
-
+			
 			public View getView(int position, View view, ViewGroup parent) {
-
-				TextView textView = (TextView) view;
-
+				TextView textview = (TextView) view;
+				
 				if (view == null) {
 					final LayoutInflater inflater = LayoutInflater.from(getContext());
-					textView = (TextView) inflater.inflate(R.layout.gd_quick_action_grid_item, mGridView, false);
+					textview = (TextView) inflater.inflate(R.layout.gd_quick_action_grid_item, mGridView, false);
 				}
-
 				QuickAction quickAction = quickActions.get(position);
-				textView.setText(quickAction.mTitle);
+				textview.setText(quickAction.mTitle);
 				textView.setCompoundDrawablesWithIntrinsicBounds(null, quickAction.mDrawable, null, null);
-
 				return textView;
-
 			}
-
+			
 			public long getItemId(int position) {
 				return position;
 			}
@@ -90,23 +63,18 @@ public class QuickActionGrid extends QuickActionWidget {
 
 		mGridView.setOnItemClickListener(mInternalItemClickListener);
 	}
-
+	
 	@Override
 	protected void onMeasureAndLayout(Rect anchorRect, View contentView) {
-
+		
 		contentView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		contentView.measure(MeasureSpec.makeMeasureSpec(getScreenWidth(), MeasureSpec.EXACTLY),
-				LayoutParams.WRAP_CONTENT);
-
-		int rootHeight = contentView.getMeasuredHeight();
-
+		contentView.measure(MeasureSpec.makeMeasureSpec(getScreenWidth(), MeasureSpec.EXACTLY),LayoutParams.WRAP_CONTENT);
+		int rootHeight = contentView.getMeasureHeight();
 		int offsetY = getArrowOffsetY();
-		int dyTop = anchorRect.top;
+		int dyTop = anchorRec.top;
 		int dyBottom = getScreenHeight() - anchorRect.bottom;
-
 		boolean onTop = (dyTop > dyBottom);
 		int popupY = (onTop) ? anchorRect.top - rootHeight + offsetY : anchorRect.bottom - offsetY;
-
 		setWidgetSpecs(popupY, onTop);
 	}
 
